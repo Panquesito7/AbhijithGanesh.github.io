dc049c1f2fc72337666873533f91f31f9041c5d5
Abhijith Ganesh <g.abhijtih14122003@gmail.com>
2021-06-11 14:56:59 +0530

    Changes

--- src/Components/Routes.jsx
+++ src/Components/Routes.jsx
@@ -4,23 +4,21 @@ import Blog from "./Blog"
 import Recom from "./Recom"
 import ContactMe from "./ContactMe"
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Link,
     Switch,
     Route,
-    HashRouter,
+    
 } from "react-router-dom";
 
 const RoutingMap = () => {
     return (
         <>
-            <Router>
-                <Switch>
-                    <Route  exact path = "/" ><Blog/></Route>
-                    <Route  exact path = "/Profile"><Profile/></Route>
-                    <Route  exact path = "/ContactMe" ><ContactMe/></Route>
-                    <Route exact path = "/Recommendations" ><Recom/></Route>
-                </Switch>
+        <Router>
+                    <Route  exact path = "/"><Blog/></Route>
+                    <Route exact path = "/Profile"><Profile/></Route>
+                    <Route exact path = "/ContactMe"><ContactMe/></Route>
+                    <Route exact path = "/Recommendations"><Recom/></Route>
             </Router>          
         </>
     )
